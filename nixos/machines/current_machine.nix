42a3cd171bf1270710a238e850887a75140481a8
jkimmerling <80171344+jkimmerling@users.noreply.github.com>
2021-03-18 15:06:55 -0400

    Update current_machine.nix

--- nixos/machines/current_machine.nix
+++ nixos/machines/current_machine.nix
@@ -25,4 +25,31 @@
 #    fsType = "ext4";
 #  };
 
+####   NVIDIA    ####
+  services.xserver.videoDriver = "nvidia";
+
+  hardware = {
+    nvidia.modesetting.enable = true;
+  };
+
+  services.picom = {
+    backend = "xrender";
+  };
+
+  services.xserver = {
+    deviceSection = ''
+      Option "NoLogo" "1"
+    '';
+
+    screenSection = ''
+      Option "TripleBuffer" "1"
+    '';
+
+    extraConfig = ''
+      Section "Extensions"
+        Option "Composite" "Enable"
+      EndSection
+    '';
+  };
+
 }
