fb56970f44ea39dce88e0f76abf9521bd24e198b
jkimmerling <80171344+jkimmerling@users.noreply.github.com>
2021-03-18 14:56:11 -0400

    Update current_machine.nix

--- nixos/machines/current_machine.nix
+++ nixos/machines/current_machine.nix
@@ -1,1 +1,28 @@
 
+{ config, pkgs, ... }:
+
+{
+  # Use the GRUB 2 boot loader.
+  boot = {
+    kernelPackages = pkgs.linuxPackages_5_8;
+    loader = {
+      grub = {
+        enable  = true;
+        device = "/dev/nvme0n1"; # or "nodev" for efi only
+        version = 2;
+      };
+    };
+  };
+
+  networking = {
+    hostName = "jasonk-home";
+    interfaces.wlp82s0.useDHCP = true;
+    interfaces.enp0s31f6.useDHCP = true;
+  };
+
+#  fileSystems."/data" = {
+#    device = "/dev/nvme0n1p3";
+#    fsType = "ext4";
+#  };
+
+}
